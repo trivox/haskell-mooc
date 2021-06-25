725406b510cbd98c2b2fcc3cdb8eb6d7f20348af
Alexander Tikhomirov <trivox@gmail.com>
2021-06-25 14:27:08 +0000

    Add Set2b

--- exercises/Set2b.hs
+++ exercises/Set2b.hs
@@ -16,7 +16,11 @@ import Data.List
 -- Hint! pattern matching is your friend.
 
 binomial :: Integer -> Integer -> Integer
-binomial = todo
+binomial n 0 = 1
+binomial 0 k = 0
+binomial n k = if (n > 0) && (k > 0) 
+               then (binomial (n-1) k) + (binomial (n-1) (k-1))
+               else 0
 
 ------------------------------------------------------------------------------
 -- Ex 2: implement the odd factorial function. Odd factorial is like
@@ -27,7 +31,14 @@ binomial = todo
 --   oddFactorial 6 ==> 5*3*1 ==> 15
 
 oddFactorial :: Integer -> Integer
-oddFactorial = todo
+oddFactorial 0 = 0
+oddFactorial 1 = 1
+oddFactorial n 
+    | n < 0     = 0
+    | odd n     = n * oddFactorial (n-2)
+    | otherwise = oddFactorial (n-1)
+
+ 
 
 ------------------------------------------------------------------------------
 -- Ex 3: implement the Euclidean Algorithm for finding the greatest
@@ -59,7 +70,13 @@ oddFactorial = todo
 -- * https://en.wikipedia.org/wiki/Euclidean_algorithm
 
 myGcd :: Integer -> Integer -> Integer
-myGcd = todo
+myGcd a b = 
+    if   b > a
+    then myGcd b a
+    else if r == 0
+         then b
+         else myGcd b r
+    where r = a `rem` b 
 
 ------------------------------------------------------------------------------
 -- Ex 4: Implement the function leftpad which adds space characters
@@ -75,7 +92,13 @@ myGcd = todo
 -- * you can compute the length of a string with the length function
 
 leftpad :: String -> Int -> String
-leftpad = todo
+leftpad str l = 
+    (
+        if pad > 0
+        then (take pad (repeat ' '))
+        else ""
+    ) ++ str
+    where pad = l - (length str)
 
 ------------------------------------------------------------------------------
 -- Ex 5: let's make a countdown for a rocket! Given a number, you
@@ -90,8 +113,13 @@ leftpad = todo
 -- * you can use the show function to convert a number into a string
 -- * you'll probably need a recursive helper function
 
+myShow :: Integer -> String
+myShow i = (show i) ++ "..."
+
 countdown :: Integer -> String
-countdown = todo
+countdown i = "Ready! " ++ 
+              (intercalate " " (map myShow [i,(i-1)..1])) ++ 
+              " Liftoff!"
 
 ------------------------------------------------------------------------------
 -- Ex 6: implement the function smallestDivisor that returns the
@@ -108,8 +136,15 @@ countdown = todo
 --
 -- Hint: remember the mod function!
 
+findDivisor :: Integer -> Integer -> Integer
+findDivisor n k 
+    | k < n     = if (n `mod` k) > 0 then findDivisor n (k+1) else k
+    | k == n    = k
+    | otherwise = findDivisor k n
+
 smallestDivisor :: Integer -> Integer
-smallestDivisor = todo
+smallestDivisor n = findDivisor n 2
+
 
 ------------------------------------------------------------------------------
 -- Ex 7: implement a function isPrime that checks if the given number
@@ -118,7 +153,10 @@ smallestDivisor = todo
 -- Ps. 0 and 1 are not prime numbers
 
 isPrime :: Integer -> Bool
-isPrime = todo
+isPrime 0 = False
+isPrime 1 = False
+isPrime n = (smallestDivisor n) == n
+            
 
 ------------------------------------------------------------------------------
 -- Ex 8: implement a function biggestPrimeAtMost that returns the
@@ -133,4 +171,5 @@ isPrime = todo
 --   biggestPrimeAtMost 10 ==> 7
 
 biggestPrimeAtMost :: Integer -> Integer
-biggestPrimeAtMost = todo
+biggestPrimeAtMost n = 
+    if isPrime n then n else biggestPrimeAtMost (n-1)
